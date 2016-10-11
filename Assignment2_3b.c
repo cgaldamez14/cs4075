a690388f57c726d88aa9b7ab842131a34949fd8a
Carlos Galdamez <cgaldamez14@gmail.com>
2016-10-11 11:54:01 -0700

    changed read functions to parallel functions

--- Assignment2_3b.c
+++ Assignment2_3b.c
@@ -1,53 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <mpi.h>
 
-void getN (int* n);
-void getElements (int* elements, int n);
-void printPrefixSum (int* prefixSums, int num );
+void get_n (int* n, int my_rank);
+void get_elements (int* elements, int* local_elements, int n, MPI_Datatype type, int my_rank);
+void print_prefix_sum (int* prefixSums, int num );
 
 int main (void){
-	int numberOfElements;
-	int* elements;
-	int* prefixSums;
+	int number_of_elements, local_n;
+	int my_rank, comm_size;
+	int *elements, *local_elements, *prefix_sums;
+	MPI_Datatype type;
+
+	MPI_Init(NULL,NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	
-	getN(&numberOfElements);
+	get_n(&number_of_elements, my_rank);
 
-	elements = malloc(numberOfElements * sizeof(int));
-	prefixSums = malloc(numberOfElements * sizeof(int));
+	local_n = number_of_elements / comm_size;
 
-	getElements(elements,numberOfElements);
+	MPI_Type_contiguous(local_n,MPI_INT, &type);
+	MPI_Type_commit(&type);
 
-	int sum = 0;
-	for (int i = 0; i < numberOfElements; i++){
-		sum += elements[i];
-		prefixSums[i] = sum;
-	}
+	elements = malloc(number_of_elements * sizeof(int));
+	prefix_sums = malloc(number_of_elements * sizeof(int));
+	local_elements = malloc(local_n * sizeof(int));
 
-	printPrefixSum(prefixSums,numberOfElements);
+	get_elements(elements, local_elements, number_of_elements, type, my_rank);
 
-	free(prefixSums);
+	//int sum = 0;
+	//for (int i = 0; i < number_of_elements; i++){
+	//	sum += elements[i];
+	//	prefix_sums[i] = sum;
+	//}
+
+	//print_prefix_sum(prefix_sums,number_of_elements);
+
+	free(prefix_sums);
 	free(elements);
+
+	MPI_Finalize();
 	return 0;
 }
 
-void getN (int* n){
-	printf("How many element do you want to get the prefix sums for?\n");
-	scanf("%d",n);
+void get_n (int* n, int my_rank){
+
+	if (my_rank == 0){
+		printf("How many element do you want to get the prefix sums for?\n");
+		scanf("%d",n);
+	}
+
+	MPI_Bcast(n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 }
 
-void getElements (int* elements, int n){
+void get_elements (int* elements, int* local_elements, int n, MPI_Datatype type, int my_rank){
 
-	printf("Enter %d integers:\n", n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &elements[i]);
+	if (my_rank == 0){
+		printf("Enter %d integers:\n", n);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &elements[i]);
+	}
 
+	MPI_Scatter(elements, 1, type, local_elements, 1, type, 0, MPI_COMM_WORLD);
 }
 
-void printPrefixSum (int* prefixSums, int num ){
+void print_prefix_sum (int* prefix_sums, int num ){
 
 	printf("Prefix Sums: ");
 	for (int i = 0; i < num; i++)
-		printf("%d ",prefixSums[i]);
+		printf("%d ",prefix_sums[i]);
 	printf("\n");
 
 }
