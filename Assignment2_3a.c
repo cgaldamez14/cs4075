4b861dbf7e5dffd7ef95dcf866ab07c0d28c1385
Carlos Galdamez <cgaldamez14@gmail.com>
2016-10-12 10:21:17 -0700

    added comments

--- Assignment2_3a.c
+++ Assignment2_3a.c
@@ -1,3 +1,11 @@
+/*
+   Sum of Prefix
+   Author: Carlos Galdamez
+   Description: Serial program that finds prefix sums of a list of numbers. For example:
+
+   If we have the list : 1 2 3 4 , then the prefix sums list would be 1, 1+2, 1+2+3, 1+2+3+4.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,19 +18,24 @@ int main (void){
 	int* elements;
 	int* prefixSums;
 	
+	// Ask user for number of elements that will be entered
 	getN(&numberOfElements);
 
+	// Allocate memory to hold list values and prefix sums
 	elements = malloc(numberOfElements * sizeof(int));
 	prefixSums = malloc(numberOfElements * sizeof(int));
 
+	// Ask use for elements of the list
 	getElements(elements,numberOfElements);
 
+	// The following creates the list of sum prefixes
 	int sum = 0;
 	for (int i = 0; i < numberOfElements; i++){
 		sum += elements[i];
 		prefixSums[i] = sum;
 	}
 
+	// Display results
 	printPrefixSum(prefixSums,numberOfElements);
 
 	free(prefixSums);
